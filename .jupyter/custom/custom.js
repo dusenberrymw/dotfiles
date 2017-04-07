46e05d60f2df31521368a9dfd38031caf1fa9eea
Mike Dusenberry <mwdusenb@us.ibm.com>
2017-04-07 12:52:27 -0700

    Adding Vim bindings to Jupyter.
    Using: https://github.com/lambdalisue/jupyter-vim-binding

---
+++ .jupyter/custom/custom.js
@@ -0,0 +1,13 @@
+// Configure CodeMirror
+require([
+  'nbextensions/vim_binding/vim_binding',   // depends your installation
+], function() {
+  // Map jj to <Esc>
+  CodeMirror.Vim.map("jj", "<Esc>", "insert");
+  // Map ':' to ';' for convenience
+  CodeMirror.Vim.map(";", ":", "normal");
+  // Swap j/k and gj/gk (Note the <Plug> mappings)
+  CodeMirror.Vim.map("j", "<Plug>(vim-binding-gj)", "normal");
+  CodeMirror.Vim.map("k", "<Plug>(vim-binding-gk)", "normal");
+});
+
