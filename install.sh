972be0cb6b3ea638bb7fa79955cab0d1981a0988
Mike Dusenberry <mwdusenb@us.ibm.com>
2017-04-13 12:48:32 -0700

    Another minor improvement to the install script.

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ FILES=!(.|..|.git|.gitignore|README.md|$SCRIPT)
 #echo $FILES
 for f in $FILES; do
   pushd ~ > /dev/null  # quiet!
-  ln -sfF $DIR/$f
-  echo "Added ~/$f -> $DIR/$f"
+  ln -sfFv $DIR/$f
   popd > /dev/null  # quiet!
 done
 
