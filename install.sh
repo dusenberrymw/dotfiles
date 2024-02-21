4659be4bca20e2a740260ccf9dba9bc090006ccf
Mike Dusenberry <dusenberrymw@gmail.com>
2024-02-20 17:24:44 -0800

    fixes install

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@
 shopt -s dotglob extglob
 DIR="$(cd "$(dirname "$0")" && pwd)"
 SCRIPT=`basename "$0"`
-FILES=!(.|..|.git|.gitignore|README.md|$SCRIPT|.DS_Store|.bashrc|.bash_profile)
+FILES=!(.|..|.git|.gitignore|README.md|$SCRIPT|.DS_Store|.bashrc|.bash_profile|Solarized*)
 echo $FILES
 for f in $FILES; do
   pushd ~ > /dev/null  # quiet!
-  ln -sfFv $DIR/$f
+  ln -sfFv "${DIR}/${f}"
   popd > /dev/null  # quiet!
 done
 # these require hard links due to some bash issue during ssh shells.
 for f in ".bashrc" ".bash_profile"; do
   pushd ~ > /dev/null  # quiet!
-  ln -fFv $DIR/$f
+  ln -fFv "${DIR}/${f}"
   popd > /dev/null  # quiet!
 done
